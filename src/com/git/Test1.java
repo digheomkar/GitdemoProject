7e1dbec9c6b1562bfcb0e873e78c246380418f12
dighe.omkar@yahoo.com <dighe.omkar@gmail.com>
2022-03-27 19:00:39 +0530

    first commite

---
+++ src/com/git/Test1.java
@@ -0,0 +1,15 @@
+package com.git;
+
+public class Test1 {
+	public static void main(String[] args) {
+		
+		System.out.println("omkar");
+		System.out.println("om");
+		System.out.println("O");
+		
+	}
+
+
+	}
+
+}
